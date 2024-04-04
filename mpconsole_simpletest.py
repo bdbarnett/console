a171da007681f4502ef06c9e302a14fce5ea5fb2
Brad Barnett <bbarnett@gmail.com>
2024-04-04 03:37:02 -0500

    wip: change simpletest to work on more platforms

--- mpconsole_simpletest.py
+++ mpconsole_simpletest.py
@@ -5,34 +5,30 @@ Copy the contents of this file to your boot.py to have it run on every bootup.
 Call `console.hide()` before you use the display for anything else.
 """
 
-try:
-    # Make sure MPDisplay and MPDisplay are available
-    from board_config import display_drv
-    from mpconsole import Console
-    try:
-        # Check to see if tft_graphics is available, because it has better fonts than base MPDisplay
-        import vga2_8x16 as font  # Check the font first, otherwise no need to load tft_graphics
-        from tft_graphics import Graphics  # Load tft_graphics
+# Make sure MPDisplay and MPDisplay are available
+from board_config import display_drv
+from mpconsole import Console
+# try:
+    # Check to see if tft_graphics is available, because it has better fonts than base MPDisplay
+    # import vga2_8x16 as font  # Check the font first, otherwise no need to load tft_graphics
+    # from tft_graphics import Graphics  # Load tft_graphics
 
-        tft = Graphics(display_drv, rotation=0)  # Create the tft object, specifying the rotation
-        # Tell Console to use tft.text as the character writer.  Have to use a lambda to map the
-        # way Console calls char_writer (without `font`) to the way tft.expects it with `font`
-        char_writer = lambda char, x, y, fg, bg: tft.text(font, char, x, y, fg, bg)
-        console = Console(tft, char_writer, cwidth=font.WIDTH, lheight=font.HEIGHT)
-    except ImportError:
-        # If tft_graphics can't be loaded, just use the base MPDisplay with base font
-        print("\nError loading tft_graphics or font.  Using base MPDisplay for console.\n")
-        console = Console(display_drv)
-except ImportError:
-    print("\nError loading MPDisplay or MPConsole.\n")
-    console = None
+    # tft = Graphics(display_drv, rotation=0)  # Create the tft object, specifying the rotation
+    # Tell Console to use tft.text as the character writer.  Have to use a lambda to map the
+    # way Console calls char_writer (without `font`) to the way tft.expects it with `font`
+    # char_writer = lambda char, x, y, fg, bg: tft.text(font, char, x, y, fg, bg)
+    # console = Console(tft, char_writer, cwidth=font.WIDTH, lheight=font.HEIGHT)
+# except ImportError:
+    # If tft_graphics can't be loaded, just use the base MPDisplay with base font
+    # print("\nError loading tft_graphics or font.  Using base MPDisplay for console.\n")
+console = Console(display_drv)
 
 
 if console:
     from sys import implementation, platform
     console.sb_text_left(platform)
 
-    if platform != "linux":
+    if implementation.name == "micropython":
         from gc import mem_free
         console(True)  # Enable the REPL
         maj, min, *_ = implementation.version
