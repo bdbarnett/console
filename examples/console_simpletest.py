01b91bb2a6877432c2b71656d9596c69281ae752
Brad Barnett <127794626+bdbarnett@users.noreply.github.com>
2024-04-27 19:11:54 -0500

    Update and rename mpconsole_simpletest.py to console_simpletest.py

--- examples/console_simpletest.py
+++ examples/console_simpletest.py
@@ -1,11 +1,11 @@
 """
-mpconsole_simpletest.py
+console_simpletest.py
 
 Call `console.hide()` before you use the display for anything else.
 """
 
 from board_config import display_drv
-from mpconsole import Console
+from console import Console
 
 
 console = Console(display_drv)
